b30da4fca8dd7a5f4fb6f54d8a6ba0fc0cd9bda8
modusss <fillype1@hotmail.com>
2025-04-19 10:07:58 -0300

    Add image attachment and friendly ID support to Product model
    - Added ActiveStorage association for image attachment.
    - Extended FriendlyId for slugged URLs based on product name.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    has_one_attached :image       # ActiveStorage
+    extend FriendlyId             # se usar FriendlyId
+    friendly_id :name, use: :slugged
 end
